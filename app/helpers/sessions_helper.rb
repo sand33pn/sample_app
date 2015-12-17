9296b6a836d5be0e3ff721d57a304b9250022f78
sand33pn <grayghost.2003@gmail.com>
2015-12-18 01:03:24 +0530

    Complete Section 9.2: Authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,12 @@ module SessionsHelper
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
-		cookies.permanent[:remember_token] = user.remember_token		
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	# returns true if the given user is the current user.
+	def current_user?(user)
+		user == current_user
 	end
 
 	# returns the current logged-in user (if any)
@@ -43,4 +48,16 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+	  # redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+	
 end
