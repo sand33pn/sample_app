a06beab31e6448628992c7af5937f6b97f16ca97
sand33pn <grayghost.2003@gmail.com>
2015-12-20 01:10:14 +0530

    Complete Section 10.1.1: Account activations resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token, :activation_token
+	before_save 	:downcase_email
+	before_create	:create_activation_digest
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
@@ -15,28 +16,41 @@ class User < ActiveRecord::Base
 	 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-	 end 
+	end 
 
-	 # returns a random token.
-	 def User.new_token
-	 	SecureRandom.urlsafe_base64
-	 end
+	# returns a random token.
+	def User.new_token
+	  SecureRandom.urlsafe_base64
+	end
 
-	 # remembers a user in the database for use in persistent sessions.
-	 def remember
+	# remembers a user in the database for use in persistent sessions.
+	def remember
 	 	self.remember_token = User.new_token
 	 	update_attribute(:remember_digest, User.digest(remember_token))
-	 end
+	end
 
-	 # returns true if the given token matches the gigest.
-	 def authenticated?(remember_token)
+	# returns true if the given token matches the gigest.
+	def authenticated?(remember_token)
 	 	return false if remember_digest.nil?
 	 	BCrypt::Password.new(remember_digest).is_password?(remember_token)	
-	 end
+	end
 
-	 # forgets a user.
-	 def forget
+	# forgets a user.
+	def forget
 	 	update_attribute(:remember_digest, nil)
-	 end
+	end
+
+	private
+
+		# converts email to all lower-case.
+		def downcase_email
+			self.email = email.downcase
+		end
+
+		# creates and assigns the activation token and digest.
+		def create_activation_digest
+			self.activation_token  = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 
 end
